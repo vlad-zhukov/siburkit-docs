80ab0e7902df91aca875df81c3c3ea28346c9f6a
siburkit <siburkit-docs>
2022-04-27 07:12:21 +0000

    Sync 8d99feb77b0b1263106f8068ee0a10c7cb1dd7c1

---
+++ docs/assets/index.21c7e125.js
@@ -0,0 +1,1 @@
+import{aX as e}from"./index.4b71c7e1.js";var o=Object.freeze(Object.defineProperty({__proto__:null,Link:e},Symbol.toStringTag,{value:"Module"}));export{o as _};
--- docs/assets/index.2dff2800.js
+++
@@ -1,1 +0,0 @@
-import{b5 as e,b6 as r,b7 as o}from"./index.2eb2ab4d.js";var _=Object.freeze(Object.defineProperty({__proto__:null,Menu:e,MenuItem:r,Scroller:o},Symbol.toStringTag,{value:"Module"}));export{_};
